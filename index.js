abb29c09847878f2be534e2d722a5c284394e89c
vijethpoojary85 <vijethpoojary225@gmail.com>
2024-02-29 21:56:42 +0530

    Product model added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Product = require
 const app = express()
 
-
+app.use(express.json());
 
 
 app.get('/',(req,res)=>{
     res.send("hello from this is node api updated");
 });
+
+app.post('/api/products',(req,res)=>{
+    try{
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+
 
 
 mongoose.connect("mongodb+srv://admin225:3wMEbKEX3T68mnwy@backenddb.difes2w.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
---
+++ models/product.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const ProductSchema = mongoose.Schema(
+    {
+name:{
+    type:String,
+    required:[true,"Product is name is required"]
+},
+quantity:{
+    type:Number,
+    required:true,
+    default :0
+},
+price:{
+    type:Number,
+    required:true,
+    default :0
+},
+img:{
+    type:String,
+    required:false
+
+},
+    },
+    {
+        timestamps:true
+    }
+);
+
+const Product = mongoose.model("Product",ProductSchema);
+module.exports = Product;

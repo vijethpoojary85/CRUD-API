2b86aab0b9c35d7bb4181b274e2ca5a29a2f58ea
vijethpoojary85 <vijethpoojary225@gmail.com>
2024-02-29 21:15:37 +0530

    mongodb added and connection done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 
-app.listen(3000,() =>{
-console.log("server is running");
-});
+
 
 app.get('/',(req,res)=>{
     res.send("hello from this is node api updated");
+});
+
+
+mongoose.connect("mongodb+srv://admin225:3wMEbKEX3T68mnwy@backenddb.difes2w.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(()=>{
+console.log("Connected to the database!");
+app.listen(3000,() =>{
+    console.log("server is running");
+    });
+})
+.catch(()=>{
+   console.log("Connection failed!") 
 });

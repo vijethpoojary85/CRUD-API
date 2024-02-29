9b8de1c46eb41e347a466804844ab4fcd5bd7464
vijethpoojary85 <vijethpoojary225@gmail.com>
2024-02-29 22:13:19 +0530

    Product data is successfully passed to the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const Product = require
+const Product = require('./models/product.model.js');
 const app = express()
 
 app.use(express.json());
@@ -10,9 +10,10 @@ app.get('/',(req,res)=>{
     res.send("hello from this is node api updated");
 });
 
-app.post('/api/products',(req,res)=>{
+app.post('/api/products',async(req,res)=>{
     try{
-
+const product=await Product.create(req.body);
+res.status(200).json(product);
     }catch(error){
         res.status(500).json({message:error.message});
     }

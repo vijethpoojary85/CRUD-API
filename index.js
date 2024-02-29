1398726278704fdd22a4f2c04aa167ba6e085130
vijethpoojary85 <vijethpoojary225@gmail.com>
2024-02-29 23:01:43 +0530

    create,update,add product done

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get('/',(req,res)=>{
     res.send("hello from this is node api updated");
 });
 
+app.get('/api/products',async(req,res)=>{
+try{
+    const product=await Product.find({});
+    res.status(200).json(product);
+}catch(error){
+    res.status(500).json({message:error.message});
+}
+});
+
+app.get('/api/products/:id',async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const product=await Product.findById(id);
+        res.status(200).json(product);
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+    });
+
+
+
 app.post('/api/products',async(req,res)=>{
     try{
 const product=await Product.create(req.body);
@@ -19,7 +41,23 @@ res.status(200).json(product);
     }
 });
 
+//update a product
 
+app.put('/api/products/:id',async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const product=await Product.findByIdAndUpdate(id,req.body);
+        if(!product){
+        return res.status(404).json({message:"product not found"});
+        }
+        const updateProduct = await Product.findById(id);
+        res.status(200).json(updateProduct);
+
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
 
 
 mongoose.connect("mongodb+srv://admin225:3wMEbKEX3T68mnwy@backenddb.difes2w.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
